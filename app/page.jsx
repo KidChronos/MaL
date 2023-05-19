b6881dfaa52a0add74e7105b0f3dce939dfe7491
Chronos <omar.benjamin377@gmail.com>
2023-05-19 15:02:18 +0000

    Update global.css and page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -29,9 +29,11 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
         {/* jumbo */}
         <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
+          <p className='w-full uppercase text-white'>Next + React Three Fiber</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight text-white'>Next 3D Starter</h1>
+          <p className='mb-8 text-2xl leading-normal text-white'>
+            A minimalist starter for React, React-three-fiber and Threejs.
+          </p>
         </div>
 
         <div className='w-full text-center md:w-3/5'>
@@ -47,8 +49,8 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
         {/* first row */}
         <div className='relative h-48 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
-          <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-white'>Events are propagated</h2>
+          <p className='mb-8 text-white'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
         </div>
         <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
           <View orbit className='relative h-full  sm:h-48 sm:w-full'>
@@ -68,8 +70,8 @@ export default function Page() {
           </View>
         </div>
         <div className='w-full p-6 sm:w-1/2'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h2>
-          <p className='mb-8 text-gray-600'>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-white'>Dom and 3D are synchronized</h2>
+          <p className='mb-8 text-white'>
             3D Divs are renderer through the View component. It uses gl.scissor to cut the viewport into segments. You
             tie a view to a tracking div which then controls the position and bounds of the viewport. This allows you to
             have multiple views with a single, performant canvas. These views will follow their tracking elements,
